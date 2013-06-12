bfdc25ec3fdf02b9974f5aab7b462f7f957450ee
Meikel Brandmeyer <mb@kotka.de>
2013-06-12 23:30:26 +0200

    Add ConfigureUtil similar to the gradle one, but public

---
+++ src/main/groovy/kotka/gradle/utils/ConfigureUtil.groovy
@@ -0,0 +1,17 @@
+package kotka.gradle.utils
+
+class ConfigureUtil {
+    static configure(Object target, Closure configureFn={}) {
+        def fn = configureFn.clone()
+
+        fn.with {
+            resolveStrategy = Closure.DELEGATE_FIRST
+            delegate        = target
+        }
+
+        if (fn.maximumNumberOfParameters == 0)
+            fn.call()
+        else
+            fn.call(target)
+    }
+}
